82edb8eebc9a45a79f78ec131dbcb33dbbd647ac
Jeffu0314 <111996311+Jeffu0314@users.noreply.github.com>
2026-03-04 14:20:47 +0800

    Practical_6 flocking done

---
+++ Practical_7/Assets/Script/flock.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flock : MonoBehaviour
+{
+    float speed = 0.001f;
+    float rotationSpeed = 5.0f;
+    Vector3 averagePosition;
+
+    float neighbourDistance = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(0.1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 5) < 1)
+            ApplyRules();
+
+        transform.Translate(0,0,Time.deltaTime * speed);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Seagull");
+
+        Vector3 vcenter = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0;
+
+        Vector3 wind = new Vector3(1, 0, 1);
+        Vector3 goalPos = GlobalFlock.goalPos;
+
+        float dist = 0;
+        int groupSize = 0;
+
+        foreach(GameObject go in gos)
+        {
+            if(go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+                if (dist <= neighbourDistance)
+                {
+                    vcenter += go.transform.position;
+                    groupSize++;
+                    if(dist < 0.5)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+                    gSpeed = gSpeed + go.GetComponent<flock>().speed;
+                }
+            }    
+        }
+        if(groupSize != 0)
+        {
+            vcenter = vcenter / groupSize + wind + (goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+            var direction = (vcenter + vavoid) - transform.position;
+
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                    Quaternion.LookRotation(direction),
+                    rotationSpeed * Time.deltaTime);
+        }
+    }
+}
