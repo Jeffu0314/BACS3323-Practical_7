d9e61a6ea8449fc6ea2b6229a340e14d8dfe2972
Jeffu0314 <111996311+Jeffu0314@users.noreply.github.com>
2026-03-16 11:44:12 +0800

    P8 navmesh

---
+++ Practical_7/Assets/Script/NavMesh/AIControl.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+}
